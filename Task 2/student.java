30d8bdccc4349f74a0734e3c895dc65c80dca134
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-19 10:32:11 +0530

    Task 2

--- Task 2/student.java
+++ Task 2/student.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 public class student{
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        
+
+        System.out.println("Enter First Subject Marks");
+        double subject_1 = sc.nextDouble();
+        System.out.println("Enter Second  Subject Marks");
+        double subject_2 = sc.nextDouble();
+        System.out.println("Enter Third Subject Marks");
+        double subject_3 = sc.nextDouble();
+        System.out.println("Enter fouth subject marks");
+        double subject_4 = sc.nextDouble();
+
+        double total = subject_1+subject_2+subject_3+subject_4;
+        double avg = total/4;
+        System.out.println("Total Marks is : "+total);
+        System.out.println("Avarage is : "+avg);
+
+        sc.close();
     }
 }
