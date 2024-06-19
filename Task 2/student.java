24ab6c3ff6ba1d91e83311a0a66ea67d5be42b95
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-19 10:44:49 +0530

    Task 2

--- Task 2/student.java
+++ Task 2/student.java
@@ -14,6 +14,22 @@ public class student{
 
         double total = subject_1+subject_2+subject_3+subject_4;
         double avg = total/4;
+
+        if(avg>=75){
+            System.out.println("Student Grade is A");
+        }
+        else if(avg>=65){
+            System.out.println("Student Grade is B");
+        }
+        else if(avg>=55){
+            System.out.println("Student Grade is C");
+        }
+        else if(avg>=35){
+            System.out.println("Student Grade is S");
+        }
+        else{
+            System.out.println("Better Luck next time");
+        }
         System.out.println("Total Marks is : "+total);
         System.out.println("Avarage is : "+avg);
 
