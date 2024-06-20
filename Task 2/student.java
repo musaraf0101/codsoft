2ca9860dbd79d40fb8c8a6e8d9d600d56c9ddde1
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-20 15:59:37 +0530

    Task 2

--- Task 2/student.java
+++ Task 2/student.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 public class student {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+
+        int subject = sc.nextInt();
+
+        for( int i=1;i<= subject; i++){
+            System.out.println("Enter the marks of" + i + "subject : ");
+        }
+        
     }
 }
