ee79e5e53db74490b76e015188b79c2051429b20
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-20 17:10:00 +0530

    Task 3

--- Task 3/ATM.java
+++ Task 3/ATM.java
@@ -13,7 +13,20 @@ public class ATM {
             System.out.println("Incorrect password");
         }
     }
-    public static void withdraw(){}
+    public static void withdraw(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your password: ");
+        int pass = sc.nextInt();
+        if(password == pass){
+            System.out.println("Enter the amount you want to withdraw: ");
+            if(Account_balance>0&&Account_balance>amount){
+                double amount = sc.nextDouble();
+            }
+            else{
+                System.out.println("Insufficient balance");
+            }
+        }
+    }
     public static void main(String [] args){
       
     }
