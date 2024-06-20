31ddbb702d4e544bae1d735df7de816dcc9d7415
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-20 17:06:37 +0530

    Task 3

--- Task 3/ATM.java
+++ Task 3/ATM.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
-
-public class user{
-    
-}
 public class ATM {
-    public static void main(String [] args){
+    int password = 1234;
+    double Account_balance = 5000;
+    public static void balance(){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your password: ");
+        int pass = sc.nextInt();
+        if(password==pass){
+            System.out.println("Your account balance is: "+Account_balance);
+        }
+        else{
+            System.out.println("Incorrect password");
+        }
+    }
+    public static void withdraw(){}
+    public static void main(String [] args){
+      
     }
 }
