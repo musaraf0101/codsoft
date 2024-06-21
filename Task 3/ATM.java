f5d245a121952ef0567758a07d705499114b8375
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-21 10:57:35 +0530

    Task 3

--- Task 3/ATM.java
+++ Task 3/ATM.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class ATM {
-    int password = 1234;
-    double Account_balance = 5000;
+    static double  password = 1234;
+    static double Account_balance = 5000;
     public static void balance(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your password: ");
@@ -19,17 +19,31 @@ public class ATM {
         int pass = sc.nextInt();
         if(password == pass){
             System.out.println("Enter the amount you want to withdraw: ");
+            double amount;
             if(Account_balance>0&&Account_balance>amount){
-                double amount = sc.nextDouble();
+                amount = sc.nextDouble();
             }
             else{
                 System.out.println("Insufficient balance");
             }
         }
+        sc.close();
     }
 
     public static void deposit(){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your password: ");
+        int pass = sc.nextInt();
+        if(password==pass){
+            System.out.println("Enter your Deposit Amount");
+            double amount = sc.nextDouble();
+            Account_balance = Account_balance + amount;
+        }
+        sc.close();
+    }
+
+    public static void front(){
+
     }
     public static void main(String [] args){
       
